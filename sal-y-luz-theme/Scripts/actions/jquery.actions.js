874921c2e70c0f7f16dacdb001bc9de489816777
Norbert Gtz <swnorberto@gmail.com>
2018-07-29 19:42:36 -0500

    Template

---
+++ sal-y-luz-theme/Scripts/actions/jquery.actions.js
@@ -0,0 +1,175 @@
+// Variables
+var $mScroll = $('.ap-scroll'),
+		$mClose = $('.ap-btn-close'),
+		$termsData = '',
+		$termsDataIns = '',
+		$card = $('.ap-form--info'),
+		$tltip = $('.ap-field-ttip'),
+		$tltipvis = 'ap-field-ttip--visible',
+		$burguer = $('.ap-btn-menu'),
+		$burguerCls = 'ap-btn-menu--is-open',
+		$menLink = $('.ap-nav-link'),
+		$elhead = $('.ap-header'),
+		$elnav = $('.ap-header-navigation');
+
+var isFootball = false;
+
+$(function () {
+	slideTop()
+	loader();
+	explorer();
+	burguerMenu();
+
+	$('#ap-nav').onePageNav({
+		currentClass: 'ap-nav-item--active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'swing'
+	});
+
+
+	var waypoints = $('#ap-lid--estadisticas').waypoint(function(direction) {
+			countNumbers();
+		}, {
+		offset: '25%'
+	})
+
+	var waypoints = $('#ap-lid-sections').waypoint(function(direction) {
+			$elhead.toggleClass('ap-header--visible')
+		}, {
+		offset: '25%'
+	})
+
+	var waypoints = $('#ap-lid--descripcion').waypoint(function(direction) {
+			$elhead.toggleClass('ap-header--ov-white')
+		}, {
+		offset: '25%'
+	})
+
+	var waypoints = $('#ap-lid--blog').waypoint(function(direction) {
+			$elhead.toggleClass('ap-header--ov-white')
+		}, {
+		offset: '25%'
+	})
+
+	var waypoints = $('#ap-lid--ultimos-entrenamientos').waypoint(function(direction) {
+			$elhead.toggleClass('ap-header--ov-white')
+		}, {
+		offset: '25%'
+	})
+
+	playMed();
+
+
+});
+
+function countNumbers() {
+	$('.ap-txt--number').each(function () {
+		var $this = $(this);
+		jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+			duration: 1000,
+			easing: 'swing',
+			step: function () {
+				$this.text(Math.ceil(this.Counter));
+			}
+		});
+	});
+}
+
+function loader() {
+	setTimeout(function () {
+		if (!isFootball) {
+			$('.ap-modal--loading').fadeOut(function () {
+				$('.ap-main').removeClass('ap-loading').addClass('ap-main--ready');
+				$(this).removeClass('ap-modal--is-visible').removeAttr('style');
+			});
+		}
+	}, 2000);
+}
+
+function explorer() {
+	if (/MSIE 9/i.test(navigator.userAgent) || /MSIE 10/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+		$('body').addClass('ap-ie')
+	}
+	if (/Edge/i.test(navigator.userAgent)) {
+		$('body').addClass('ap-edge');
+	}
+	if (/rv:11.0/i.test(navigator.userAgent)) {
+		$('body').addClass('ap-ie-11')
+	}
+}
+
+function burguerMenu() {
+	if (detectmob()) {
+		$burguer.on('click', function(){showMenu()});
+		$menLink.on('click', function(){showMenu()});
+	}
+}
+
+function showMenu() {
+	$burguer.toggleClass($burguerCls);
+	$elnav.toggleClass('ap-header-navigation--is-open');
+	$elhead.toggleClass('ap-header--is-open');
+}
+
+function slideTop() {
+	var mySwiper = new Swiper ('.ap-art-slide-container', {
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+		},
+	})
+}
+
+// Youtube Trailer
+var player;
+
+function onYouTubeIframeAPIReady() {
+	openYouTube();
+}
+
+function start() {
+	var $media = $('.ap-media-btn').attr('data');
+
+	player = new YT.Player('player', {
+		playerVars: { 'autoplay': 1, 'showinfo': 0, 'modestbranding': 0, 'rel': 0, 'controls': 0, 'playsinline': 1 },
+		videoId: $media,
+		events: {
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+function onPlayerStateChange(event) {
+	if (event.data == 0) {
+		removeYoutube();
+	}
+}
+
+function openYouTube() {
+	start();
+}
+
+function removeYoutube() {
+	// Remove Trailer
+	$('.ap-media-btn').removeClass('ap-media-btn--hiden');
+	player.destroy();
+}
+
+function playMed() {
+	var $medLik = $('.ap-media-btn');
+
+	$medLik.on('click', function(e) {
+		e.preventDefault();
+		$(this).addClass('ap-media-btn--hiden');
+		openYouTube();
+	});
+}
---
+++ sal-y-luz-theme/header.php
@@ -0,0 +1,45 @@
+
+		<header class="ap-header">
+			<div class="ap-panel cf">
+				<a class="ap-btn-menu">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</a>
+
+				<h1 class="ap-tt-brand">
+					<a href="#" class="ap-tt-brand-link">
+						<img src="<?php bloginfo('stylesheet_directory'); ?>/Content/img/img-tt.png" alt="">
+						<span class="ap-tt-tt">
+							Sal y Luz La transformación comienza en mi.
+						</span>
+					</a>
+				</h1>
+
+				<nav class="ap-header-navigation">
+					<div class="ap-nav ap-nav--social">
+						<a href="#" class="ap-btn-sc icon-li-instagram"></a>
+						<a href="#" class="ap-btn-sc icon-li-dribbble"></a>
+						<a href="#" class="ap-btn-sc icon-li-twitter"></a>
+					</div>
+					<div class="ap-nav ap-nav--left">
+						<ul class="cf" id="ap-nav">
+							<li class="ap-nav-item ap-nav-item--active">
+								<a href="#ap-lid--top" class="ap-nav-link">Inicio</a>
+							</li>
+							<li class="ap-nav-item">
+								<a href="#ap-lid--facilitadores" class="ap-nav-link">Facilitadores</a>
+							</li>
+							<li class="ap-nav-item">
+								<a href="#ap-lid--testimonios" class="ap-nav-link">Historias</a>
+							</li>
+							<li class="ap-nav-item">
+								<a href="" class="ap-nav-link ap-nav-link--no-link">Registro
+								/ entrar</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
